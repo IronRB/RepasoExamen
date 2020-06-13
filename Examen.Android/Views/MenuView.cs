48240fe43d6adfdbf682a1dc2fdb4fc5a7368870
ROBERT ARLEY CARMONA VELASQUEZ <robertcarmona214507@correo.itm.edu.co>
2020-06-12 23:23:02 -0500

    archivos iniciales

---
+++ Examen.Android/Views/MenuView.cs
@@ -0,0 +1,18 @@
+﻿using Android;
+using Android.App;
+using Android.OS;
+using Examen.Core.ViewModels;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Examen.Android.Views
+{
+    [Activity(Label = "@string/app_name")]
+    public class MenuView : MvxActivity<MenuViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.MenuPage);
+        }
+    }
+}
---
+++ Examen.Core/app.cs
@@ -0,0 +1,20 @@
+﻿
+using Examen.Core.ViewModels;
+using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+
+namespace Examen.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<MenuViewModel>();
+        }
+    }
+}
